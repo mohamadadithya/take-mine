ba2fef00de7b985b43f1c1c93d6af6d2745726d6
Mohamad Adithya <mohamadadithya@gmail.com>
2023-08-29 10:39:16 +0700

    update button dropdown

--- src/lib/components/ButtonDropdown.svelte
+++ src/lib/components/ButtonDropdown.svelte
@@ -2,8 +2,8 @@
 	import { clickoutside } from '@svelte-put/clickoutside';
 	import type { Link } from '$lib/data/links';
 
-	export let isOpen = false,
-				links: Link[] = [];
+	export let isOpen = true,
+		links: Link[] = [];
 </script>
 
 <div class="relative">
@@ -11,19 +11,40 @@
 		<slot />
 	</button>
 	<div
-		use:clickoutside on:clickoutside={() => isOpen = false}
+		use:clickoutside
+		on:clickoutside={() => (isOpen = false)}
 		id="dropdown"
-		class="absolute {isOpen ? '' : 'hidden'} right-0 shadow-md bg-white p-4 w-52 rounded-xl"
+		class="absolute {isOpen ? '' : 'hidden'} right-0 shadow-md bg-white p-4 w-64 rounded-xl"
 	>
+		<div class="flex items-start gap-3 border-b pb-3 mb-3">
+			<img
+				src="/images/profile-1.jpg"
+				class="w-14 h-14 aspect-square rounded-full border border-gray-300"
+				alt="Username"
+			/>
+			<div>
+				<p class="font-semibold">John Doe</p>
+				<p class="text-xs">johndoe@gmail.com</p>
+				<p class="text-xs bg-primary-200 w-fit py-1 px-3 rounded-full mt-1.5">Admin</p>
+			</div>
+		</div>
 		<ul>
 			{#each links as link}
 				<li>
 					<a class="py-2 px-2.5 rounded-md block hover:bg-gray-100" href={link.path}
-						><i class="far mr-1 fa-fw {link.icon} {link.path == '/logout' ? 'rotate-180' : ''}" />
+						><i class="far mr-1 fa-fw {link.icon}" />
 						{link.name}</a
 					>
 				</li>
 			{/each}
+			<li class="border-t mt-2 pt-3">
+				<form method="POST">
+					<button type="button" class="btn btn-error w-full rounded-full">
+						Logout
+						<i class="far ml-1 fa-fw fa-sign-out text-lg" />
+					</button>
+				</form>
+			</li>
 		</ul>
 	</div>
 </div>
