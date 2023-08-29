b6d571e828fb81a50b647602fb1705262becda1f
Mohamad Adithya <mohamadadithya@gmail.com>
2023-08-29 11:28:18 +0700

    remove fontawesome cdn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	import '@fontsource/poppins/500.css';
 	import '@fontsource/poppins/600.css';
 	import '@fontsource/poppins/700.css';
-    import '$lib/app.css';
-    import Nprogress from 'nprogress';
-    import 'nprogress/nprogress.css'
+	import '$lib/app.css';
+	import Nprogress from 'nprogress';
+	import 'nprogress/nprogress.css';
 	import { navigating, updated } from '$app/stores';
 
 	Nprogress.configure({ minimum: 0.16, showSpinner: false });
@@ -15,12 +15,6 @@
 		if ($updated) window.location.reload();
 	}
 </script>
-
-<link
-	href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css"
-	rel="stylesheet"
-	type="text/css"
-/>
 
 <main class="font-poppins" data-sveltekit-reload={$updated ? '' : 'off'}>
 	<slot />
